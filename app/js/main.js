7421d8dd04af4a4b103c2b97cd417a68bc6b82cf
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-01-17 20:01:05 +0300

    added  arrows svg

--- app/js/main.js
+++ app/js/main.js
@@ -3,8 +3,10 @@ $(function () {
     dots: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow: "<button type='button' class='slick-arrow slick-next'></button>",
-    prevArrow: "<button type='button' class='slick-arrow slick-prev'></button>",
+    nextArrow:
+      "<button type='button' class='slick-arrow slick-next'><img src='../images/arrow-next.svg' alt='slider next arrow'></button>",
+    prevArrow:
+      "<button type='button' class='slick-arrow slick-prev'><img src='../images/arrow-prev.svg' alt='slider prev arrow'></button>",
     responsive: [
       {
         breakpoint: 1024,
